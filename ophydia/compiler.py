a80c552b8efec77b27e1f647ea6507fd382ddfe8
Dadiao.Moe <user@email.com>
2018-10-13 17:05:46 +0800

    Init for instantiate

--- ophydia/compiler.py
+++ ophydia/compiler.py
@@ -1,4 +1,4 @@
-# from vyper.parser import parser
+from vyper.parser import parser as vparser
 # from vyper import compile_lll
 # from vyper import optimizer
 
@@ -6,18 +6,22 @@
 def compile(code, *args, **kwargs):
     # lll = optimizer.optimize(parser.parse_tree_to_lll(parser.parse(code), code, runtime_only=kwargs.get('bytecode_runtime', False)))
     # asm = compile_lll.compile_to_assembly(lll)
-    asm = {}
 
-    def find_nested_opcode(asm_list, key):
-        if key in asm_list:
-            return True
-        else:
-            sublists = [sub for sub in asm_list if isinstance(sub, list)]
-            return any([find_nested_opcode(x, key) for x in sublists])
+    # TODO: will BVM support DEBUG opcode in the future
+    # def find_nested_opcode(asm_list, key):
+    #     if key in asm_list:
+    #         return True
+    #     else:
+    #         sublists = [sub for sub in asm_list if isinstance(sub, list)]
+    #         return any([find_nested_opcode(x, key) for x in sublists])
 
-    if find_nested_opcode(asm, 'DEBUG'):
-        print('Please note this code contains DEBUG opcode.')
-        print('This will only work in a support EVM. This FAIL on any other nodes.')
+    # if find_nested_opcode(asm, 'DEBUG'):
+    #     print('Please note this code contains DEBUG opcode.')
+    #     print('This will only work in a support BVM. This FAIL on any other nodes.')
 
     # return compile_lll.assembly_to_evm(asm)
+    return bytes(code, 'utf8')
+
+def instantiate(code, args):
+    # tree = vparser.parse(code)
     return bytes(code, 'utf8')
