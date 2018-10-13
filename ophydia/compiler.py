badf867b08d582258627c5e9e36e7135db26fbbe
Dadiao.Moe <user@email.com>
2018-10-13 09:06:15 +0800

    Read contract args

--- ophydia/compiler.py
+++ ophydia/compiler.py
@@ -6,6 +6,7 @@
 def compile(code, *args, **kwargs):
     # lll = optimizer.optimize(parser.parse_tree_to_lll(parser.parse(code), code, runtime_only=kwargs.get('bytecode_runtime', False)))
     # asm = compile_lll.compile_to_assembly(lll)
+    asm = {}
 
     def find_nested_opcode(asm_list, key):
         if key in asm_list:
@@ -14,9 +15,9 @@ def compile(code, *args, **kwargs):
             sublists = [sub for sub in asm_list if isinstance(sub, list)]
             return any([find_nested_opcode(x, key) for x in sublists])
 
-    # if find_nested_opcode(asm, 'DEBUG'):
-    #     print('Please not this code contains DEBUG opcode.')
-    #     print('This will only work in a support EVM. This FAIL on any other nodes.')
+    if find_nested_opcode(asm, 'DEBUG'):
+        print('Please note this code contains DEBUG opcode.')
+        print('This will only work in a support EVM. This FAIL on any other nodes.')
 
     # return compile_lll.assembly_to_evm(asm)
     return bytes(code, 'utf8')
