c751905f83e04b77b063349121c81dbdb7f7bed7
Dadiao.Moe <user@email.com>
2018-10-20 18:10:06 +0800

    pypy

--- ophydia/main.py
+++ ophydia/main.py
@@ -1,44 +1,46 @@
-# import ophydia.lexer as lexer
+import io
+import os
+import os.path
+import sys
+import ophydia.compiler
 
-from ophydia.errors import error_collector, CompilerError
 
-def main(file):
-    result = process_file(file)
-    error_collector.show()
-    return bytes(result, 'utf8')
+class CompileError(Exception):
+    def __init__(self, exc_type, exc_value, file, msg=''):
+        exc_type_name = exc_type.__name__
+        if exc_type is SyntaxError:
+            tbtext = ''.join(traceback.format_exception_only(
+                exc_type, exc_value))
+            errmsg = tbtext.replace('File "<string>"', 'File "%s"' % file)
+        else:
+            errmsg = "Sorry: %s: %s" % (exc_type_name,exc_value)
 
-def process_file(file):
-    code = file.read()
-    return code
+        Exception.__init__(self,msg or errmsg,exc_type_name,exc_value,file)
 
-    if not error_collector.ok():
-        return None
+        self.exc_type_name = exc_type_name
+        self.exc_value = exc_value
+        self.file = file
+        self.msg = msg or errmsg
 
-    token_list = lexer.tokenize(code, file)
-    if not error_collector.ok():
-        return None
+    def __str__(self):
+        return self.msg
 
-    token_list = preproc.process(token_list, file)
-    if not error_collector.ok():
-        return None
+def main(file, optimize=-1):
+    source_bytes = read_source(file)
+    try:
+        code = compiler.compile(source_bytes, file,
+                                     _optimize=optimize)
+    except Exception as err:
+        py_exc = CompileError(err.__class__, err, file)
+        sys.stderr.write(py_exc.msg + '\n')
+        return
 
-    # If parse() can salvage the input into a parse tree, it may emit an
-    # ast_root even when there are errors saved to the error_collector. In this
-    # case, we still want to continue the compiler stages.
-    ast_root = parse(token_list)
-    if not ast_root:
-        return None
+    # source_stats = loader.path_stats(file)
+    # bytecode = importlib._bootstrap_external._code_to_bytecode(
+    #         code, source_stats['mtime'], source_stats['size'])
+    print(bytecode)
 
-    il_code = ILCode()
-    symbol_table = SymbolTable()
-    ast_root.make_il(il_code, symbol_table, Context())
-    if not error_collector.ok():
-        return None
 
-    asm_code = ASMCode()
-    ASMGen(il_code, symbol_table, asm_code, args).make_asm()
-    asm_source = asm_code.full_code()
-    if not error_collector.ok():
-        return None
-
-    return asm_source
+def read_source(path):
+    with io.FileIO(path, 'r') as file:
+        return file.read()
